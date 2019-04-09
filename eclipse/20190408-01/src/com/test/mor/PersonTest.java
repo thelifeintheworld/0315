90007baaf14ef064cc1833a74a246ebafc019b23
renshengzaishi <1821454920@qq.com>
2019-04-09 20:40:22 +0800

    0409学习

--- eclipse/20190408-01/src/com/test/mor/PersonTest.java
+++ eclipse/20190408-01/src/com/test/mor/PersonTest.java
@@ -2,41 +2,17 @@ package com.test.mor;
 
 public class PersonTest {
 	public static void main(String[] args) {
-		Person p1 = new Person();
-		Person p2 = new Person();
-		p1.num = 1;
-		p1.name = "章飞";
-		p1.sex ="男";
-		p1.ismarry=false;
-		p2.name="带鱼";
-		p2.ismarry=false;
-		p2.sex = "女";
-		boolean a = Person.marry(p1,p2);
-		System.out.println(a);
-		// TODO Auto-generated method stub
-		System.out.print(p1.name+"的伴侣是："+p1.wh.name);
+		Person p1 = new Person(123456,"章鱼","女",23,false,"1233214566544456","深海","0351-227236",null);
+		Person p2 = new Person(123446,"带鱼","男",23,false,"1233214566588856","深海","0351-227886",p1);
+		p1.wh = p2;
+		System.out.println(p1.ismarry);
+		System.out.println(p2.ismarry);
+		  boolean marry= p1.marry(p2);
+		  System.out.println(p1.ismarry);
+			System.out.println(p2.ismarry);
+			System.out.println(marry);
+		System.out.println(p1.name+"的伴侣是："+p1.wh.name);
 	}
 
 }
 //
-class Person{
-	int num;
-	String name;
-	String sex;
-	int age;
-	boolean ismarry;
-	String idno;
-	String add;
-	String tel;
-	Person	wh;
-	public static boolean marry(Person p1,Person p2) {
-		if((p1.ismarry ==false && p2.ismarry == false) && p1.sex != p2.sex) {
-			p1.ismarry = p2.ismarry = true;
-			p1.wh = p2;
-			p2.wh =p1;
-			return true;
-		}else {
-			return false;
-		}
-	}
-}
